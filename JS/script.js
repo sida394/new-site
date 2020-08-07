541ba6ff5a4a029d7af67379cf63f3a1cfbec922
sidou <sidahmed.issolah@gmail.com>
2020-08-07 23:30:40 +0100

    modification in books info

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,49 @@
-$('.dropdown-toggle').dropdown();
+// books info
+var x = document.getElementById("book-1");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("mouseout", myFunctionTwo);
+
+function myFunction() {
+    document.getElementById("info-1").style.display = "block";
+}
+
+function myFunctionTwo() {
+    document.getElementById("info-1").style.display = "none";
+
+}
+var x = document.getElementById("book-2");
+x.addEventListener("mouseover", myFunction2);
+x.addEventListener("mouseout", myFunctionTwo2);
+
+function myFunction2() {
+    document.getElementById("info-2").style.display = "block";
+}
+
+function myFunctionTwo2() {
+    document.getElementById("info-2").style.display = "none";
+
+}
+var x = document.getElementById("book-3");
+x.addEventListener("mouseover", myFunction3);
+x.addEventListener("mouseout", myFunctionTwo3);
+
+function myFunction3() {
+    document.getElementById("info-3").style.display = "block";
+}
+
+function myFunctionTwo3() {
+    document.getElementById("info-3").style.display = "none";
+
+}
+var x = document.getElementById("book-4");
+x.addEventListener("mouseover", myFunction4);
+x.addEventListener("mouseout", myFunctionTwo4);
+
+function myFunction4() {
+    document.getElementById("info-4").style.display = "block";
+}
+
+function myFunctionTwo4() {
+    document.getElementById("info-4").style.display = "none";
+
+}
